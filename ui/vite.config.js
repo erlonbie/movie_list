7875b619ccc57ff589ab74b95397f09313cac2d7
erlonbie <erlonminievix@gmail.com>
2023-02-21 21:00:02 -0400

    frontend container setup

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // needed for the Docker Container port mapping to work
+    port: 3001, // you can replace this port with any port
+  },
 })
