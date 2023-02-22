cadf7050d8c4679dbd8ae9db467ea8f65b22e7a5
erlonbie <erlonminievix@gmail.com>
2023-02-21 20:16:23 -0400

    App working

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 function App() {
   const [movieText, setMovieText] = useState('')
   const [movieItem, setMovieItem] = useState([])
+  const [movieDelete, setMovieDelete] = useState(false)
 
   useEffect(() => {
     axios
@@ -15,39 +16,41 @@ function App() {
       .catch((error) => {
         console.error('Failed to fetch data', error)
       })
-  }, [])
+  }, [movieDelete])
 
   const addMovie = (db) => {
     if (!movieText) return alert('Preencha um filme')
-    const title = movieText.trim().toUpperCase();
+    const title = movieText.trim().toUpperCase()
     const newMovie = {
       title: title,
       body: 'Sample body',
       published: db == 'db1' ? true : false,
     }
-    axios.post(`http://localhost:8000/blog-posts/${db}`, JSON.stringify(newMovie)).then(
-      (response) => {
+    axios
+      .post(`http://localhost:8000/blog-posts/${db}`, JSON.stringify(newMovie))
+      .then((response) => {
         console.log('Success:', response.data)
-        setMovieItem([...movieItem, newMovie])
+        setMovieDelete(!movieDelete)
         setMovieText('')
-      }
-    ).catch(error => {
-        console.error('Error:', error);
+      })
+      .catch((error) => {
+        console.error('Error:', error)
         return alert('Não é permitido itens duplicados')
         // Do something with the error, like show an error message
-      });
+      })
   }
 
-  const removeTask = (id) => {
-    const newList = movieItem.filter((movieText) => movieText.id !== id)
-    setMovieItem(newList)
-  }
-
-  const toggleChecked = (id, checked) => {
-    const index = movieItem.findIndex((movieText) => movieText.id === id)
-    const newList = movieItem
-    newList[index].checked = !checked
-    setMovieItem([...newList])
+  const delteMovie = (db, title) => {
+    axios
+      .options(`http://localhost:8000/blog-posts/${db}/delete/${title}`)
+      .then(() => {
+        console.log('Movie deleted successfully')
+        setMovieDelete(!movieDelete)
+      })
+      .catch((error) => {
+        setMovieDelete(!movieDelete)
+        console.error('Failed to delete movie', error)
+      })
   }
 
   return (
@@ -61,23 +64,46 @@ function App() {
           placeholder='Digite um filme'
           onChange={(e) => setMovieText(e.target.value)}
         />
-        <Button id='bd1' onClick={() => addMovie('db1')}>BD1</Button>
-        <Button id='bd2' onClick={() => addMovie('db2')}>BD1</Button>
+        <Button className='bx bx-data' id='bd1' onClick={() => addMovie('db1')}>
+          BD1
+        </Button>
+        <Button className='bx bx-data' id='bd2' onClick={() => addMovie('db2')}>
+          BD2
+        </Button>
       </Flex>
       <Spacer margin='16px' />
 
       <ul>
-        {movieItem.map((movieText) => (
+        {movieItem?.map((movieText) => (
+          // <React.Fragment key={movieText.title}>
           <>
-            <Item Item key={movieText.id}>
+            <Item
+              // Item
+              key={movieText.title}
+              onClick={() =>
+                console.log(movieText.id, movieText.body, movieText.published)
+              }
+            >
               <p>{movieText.title}</p>
               <Flex direction='row'>
-                <button onClick={() => removeTask('db1', movieText.id) } >
-                  <i className='bx bx-trash-alt '></i>
-                </button>
-                <button onClick={() => removeTask('db2', movieText.id) } >
-                  <i className='bx bx-trash '></i>
-                </button>
+                {movieText.body == 'both' ? (
+                  <div>
+                    <button onClick={() => delteMovie('db1', movieText.title)}>
+                      <i className='bx bx-trash-alt '>1</i>
+                    </button>
+                    <button onClick={() => delteMovie('db2', movieText.title)}>
+                      <i className='bx bx-trash '>2</i>
+                    </button>
+                  </div>
+                ) : movieText.published ? (
+                  <button onClick={() => delteMovie('db1', movieText.title)}>
+                    <i className='bx bx-trash-alt '>1</i>
+                  </button>
+                ) : (
+                  <button onClick={() => delteMovie('db2', movieText.title)}>
+                    <i className='bx bx-trash '>2</i>
+                  </button>
+                )}
               </Flex>
             </Item>
             <Spacer margin='12px' />
